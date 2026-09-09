be4b14e818e6005fb531dfbf07774530c11b5312
Aseem Arora <reachaseemarora@gmail.com>
2026-09-09 11:52:16 -0600

    added calculateArea and calculateVolume

--- 01 - dart + flutter intro/ex02 - dart language exercises/student/dart-exercises/bin/dart_exercises.dart
+++ 01 - dart + flutter intro/ex02 - dart language exercises/student/dart-exercises/bin/dart_exercises.dart
@@ -1,5 +1,19 @@
 import 'package:dart_exercises/dart_exercises.dart' as dart_exercises;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_exercises.calculate()}!');
+
+double calculateArea(double length, double width, double height) {
+ return 2 * ((length * width) + (length * height) + (width * height));
+}
+
+double calculateVolume(double length, double width, double height) {
+  return length * width * height;
+}
+
+void main() {
+  double length = double.parse(stdin.readlineSync());
+  double width = double.parse(stdin.readlineSync());
+  double height = double.parse(stdin.readlineSync());
+
+  print('Surface Area: ${calculateArea(length, width, height)}');
+  print('Volume: ${calculateVolume(length, width, height)}');
 }
